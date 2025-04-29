c7b8e6755af36b1b2f307614132e9b4236565130
gubina-irina <banshchikova.i.s@gmail.com>
2025-04-29 12:31:45 +0700

    tasks completed in the 8th sprint

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,36 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Irina Gubina on 27.04.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var gradientView: UIView!
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    private func setupGradient() {
+        let darkColor = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1.0)
+        
+        gradientLayer.frame = gradientView.bounds
+        
+        gradientLayer.colors = [
+            darkColor.withAlphaComponent(0.6).cgColor,
+            darkColor.withAlphaComponent(0.0).cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.locations = [0, 1]
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
